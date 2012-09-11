da4a6319078de2dc66a68a00d43ee752ad6a71b2
Simon Nistad <simon.nistad@hotmail.com>
2012-09-11 13:14:30 +0200

    started on a com lib for sockets

--- communication.py
+++ communication.py
@@ -2,8 +2,36 @@
 import socket
 import thread
 
-class Listener():
-  def __init__(self):
+class FileServer():
+  def __init__(self, addr, port):
     self.host = socket.gethostname()
     
-    self.sock = 
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    self.sock.bind((addr, port))
+    
+    self.sock.listen(5)
+    
+  def main(self):
+    try:
+      while 1:
+	(connection, addr) = self.sock.accept()
+	print "Client connected", connection, addr
+	
+	handler = Handler(connection)
+	thread.start_new_thread(handler.main, ())
+	
+	
+class FileHandler:
+  def __init__(self, conn):
+    self.conn = conn
+    
+    self.cfile = conn.makefile('rw', 0)
+    
+  def main(self):
+    line = self.cfile.readline().strip()
+    print "--> REQUEST IS:", line
+    
+    
