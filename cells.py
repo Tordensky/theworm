45884f51cd4da70f3c979078269e75310ce8043f
Simon Nistad <simon@simon-VirtualBox.(none)>
2012-09-11 11:54:51 +0200

    setup pygame screen and fixed gitignore for tmp files

---
+++ cells.py
@@ -0,0 +1,17 @@
+import pygame 
+from pygame.locals import *
+
+pygame.init()
+
+screen_res = (1024, 768)
+
+screen = pygame.display.set_mode((screen_res), 0, 32)
+
+
+while True:
+        for event in pygame.event.get():
+                key = pygame.key.get_pressed()
+                if event.type == QUIT or key[pygame.K_ESCAPE]:
+                        exit()
+
+        pygame.display.update()
