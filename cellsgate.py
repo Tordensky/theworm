74d1de66ee60fa6bcca97197d4600f095e6461ec
Alexander Svendsen <alexandersvendsen@hotmail.com>
2012-09-17 23:27:20 +0200

    fixed a zip file transfer error, evrything should work on the filehandler part

---
+++ cellsgate.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import commands
+
+port = 30666
+ip = 'localhost';
+
+class CellGate():
+  def __init__(self):
+    pass
+  
+  def startup(self):
+    
+    """
+    Start up wormgate
+    """
+    
+    pass
+  
+  def listenForIncomingCells(self):
+    """
+    Listen for incoming segments
+    """
+    pass
+  
+    # TODO Receive new segments
+  
+  def becomeInefected(self):
+    """
+    Startup worm segment on host
+    """
+    print "Staring worm segment"
+    
+    cmd = "python cells.py"
+  
+    res, text = commands.getstatusoutput( cmd )
+    
+    print "Returned status : %d" % res
+    print "Returned text \n%s" % text
+    
+  def die(self):
+    """
+    Stop worm gate
+    """
+    pass
+  
+    # TODO kill wormgate
+  
+
+
+if __name__ == "__main__":
+  print "Staring wormgate"
+  
--- communication.py
+++ communication.py
@@ -2,31 +2,25 @@
 import socket
 import thread
 import sys
+import commands
 
 TYPE_FILE = '1'
 
 class FileServer():
-    def __init__(self, addr, port):
-        self.host = socket.gethostname()
-        
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
-        self.sock.bind((addr, port))
-        
-        self.sock.listen(5)
-        
-    def main(self):
+	def __init__(self, addr, port):
+		self.host = socket.gethostname()
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.sock.bind((addr, port))
+		self.sock.listen(5)
+	def main(self):
 		try:
-			while 1:
+			while True:
 				(connection, addr) = self.sock.accept()
-				
 				print "Client connected", connection, addr
-				
 				handler = FileHandler(connection)
-				
 				thread.start_new_thread(handler.main, ())
+				
 		except:
 			print "some kind of weird error"
 
@@ -37,19 +31,11 @@ class FileClient():
 		tmpBuffer = ""
 		try:
 			print "printing : " + filepath
-			tmpBuffer += open(filepath, 'rU+').read()
-			#data_file = File.open(filepath, 'rU')  
-			#while(True):
-				#bytes = data_file.read(5)
-				#if bytes:
-					#tmpBuffer += bytes
-				#else:
-					#break
+			tmpBuffer += open(filepath, 'r').read()
+			
 		except:
 			print "File load Error"
 			print sys.exc_info()
-		#Testign that the file is correct
-		print tmpBuffer
 		return tmpBuffer
     
 
@@ -58,7 +44,7 @@ class FileClient():
 	def sendFile(filepath, addr, port):
 		dataBuffer = FileClient.readBytesFromFile(filepath)
 		message = MessageHandler.buildMessage(dataBuffer)
-		print message
+		
 		try:    
 			cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			cs.connect((addr, port))
@@ -69,7 +55,6 @@ class FileClient():
                 
 
 class MessageHandler():
-    
 	@staticmethod
 	def buildMessage(dataBuffer):
 		message = (("Type: " + TYPE_FILE + "\n") +
@@ -102,26 +87,32 @@ class FileHandler():
     
 	def main(self):
 		dataDict = MessageHandler.DataToDict(self.cfile)
-		print dataDict["Payload:"]
-		self.saveDataToFile("/tmp/inf3200/asv009/recived.zip", dataDict["Payload:"])
+		self.saveDataToFile("/tmp/inf3200/asv009/theworm.zip", dataDict["Payload:"])
+		self.runCode()
+		print "Got data, unziped it and made it run"
     
-    
-                
+	def runCode(self):
+		res, text = commands.getstatusoutput( "unzip -o /tmp/inf3200/asv009/theworm.zip -d /tmp/inf3200/asv009" )
+		res, text = commands.getstatusoutput("python /tmp/inf3200/asv009/cells.py")
+		
 	def saveDataToFile(self, filename, data):
 		try:
-			new_file = open(filename, 'w+')
+			new_file = open(filename, 'w')
 			new_file.write(data)
-			new_file.close
+			new_file.close()
 		except:
 			print "File save error"
 			print sys.exc_info()
 		print "Saved file at: " + filename
                     
     
+#testing the communication, needs the file at the location tough
 if __name__ == "__main__":
+	#send in more arguments to make the client run, insert nothing to get the server to run
 	if len(sys.argv) == 2:
 		print "Starting Client test"
-		FileClient.sendFile("/tmp/inf3200/asv009/test.zip", 'localhost', 8080)
+		FileClient.sendFile("/tmp/inf3200/worm.zip", 'localhost', 8080)
+		
 	else:
 		print "Starting Server test"
 		server = FileServer('localhost', 8080)
