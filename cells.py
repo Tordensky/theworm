fbd662baed3b1cd9a45e66a9abfe7765482a8c69
Simon Nistad <simon.nistad@hotmail.com>
2012-10-04 16:38:48 +0200

    Refactored the wormgate and cells

--- cells.py
+++ cells.py
@@ -14,16 +14,15 @@ from pygame.color import THECOLORS
 from graphics import * 
 
 
-def ChangeRunningToFalse():
+def quitPython():
+	'''
+	Method for quiting everything
+	'''
 	global RUNNING
 	sys.stdout.flush()
-	#pygame.display.quit()
 	RUNNING = False
 	os._exit(0)
 	
-
-
-
 class WormSegment():
 	def __init__(self):
 		"""
@@ -34,43 +33,30 @@ class WormSegment():
 		self.heartbeatreciver = 0.0
 		self.udpComm = UDPcomm(MCAST_PORT)
 		self.numberOfSegments = 0
+	
 	def main(self):
 		"""
 		Running the main code for the worm
 		"""
-
 		self.listenForIncommingHeartBeats()
 		thread.start_new_thread(self.sendHeartBeat,())
-		boids = Graphics(ChangeRunningToFalse, self.get_num_segments)
+		boids = Graphics(quitPython, self.get_num_segments)
 		thread.start_new_thread(boids.run, ())
 		while RUNNING:
-			time.sleep(self.estimateHartBeatIntervall/1000.0)
+			time.sleep(self.estimateHartBeatIntervall/1000.0) # MS to seconds
 			self.estimateNumberOfWormSegment()
 			
-		
-	
-	
 	def propagate(self):
 		"""
-		This fuction is responsible for spreading itself to another node
+		This method is responsible for spreading itself to another node
 		"""
 		if RUNNING == True:
 			target = TARGET_IPS[random.randint(0, len(TARGET_IPS) - 1)]
 			communication.FileClient().sendFile( TMP_FOLDER +  "theworm.zip",target, WORM_GATE_PORT)
-			#if self.numberOfSegments <= 1:
-				#for i in range(0, BUCKET_SHOT):
-					#print i
-					#self.sendWormSegment()
-			#else:
-				#self.sendWormSegment()			
 	
-	def sendWormSegment(self):
-		target = TARGET_IPS[random.randint(0, len(TARGET_IPS) - 1)]
-		communication.FileClient().sendFile( TMP_FOLDER +  "theworm.zip",target, WORM_GATE_PORT)
-		
 	def sendHeartBeat(self):
 		"""
-		Broadcasts a heartbeat to all the rest of the worms
+		Multicasts a heartbeat to all the rest of the worms
 		"""
 		while RUNNING:
 			self.udpComm.send(self.sendHeartBeatIntervall)
@@ -81,8 +67,7 @@ class WormSegment():
 		"""
 		Estimates how many worms are in play 
 		"""
-		
-		#Will get a race condition here, but we don't care about it since it's only a estimate
+		# NOTE Will get a race condition here, but we don't care about it since it's only a estimate
 		self.numberOfSegments = self.heartbeatreciver/float(self.estimateHartBeatIntervall)
 		self.numberOfSegments = math.ceil(self.numberOfSegments)
 		self.heartbeatreciver = 0.0
@@ -95,23 +80,26 @@ class WormSegment():
 			self.shouldIPropagate(self.numberOfSegments)
 
 	def shouldIKillMyself(self, numberOfSegmentsAlive):
-		#print "number of segs alive and max", numberOfSegmentsAlive, MAX_WORM_SEGS
+		'''
+		Calculates the probability of a segment killing itself, gets a random value from 1 - 100 
+		and checks if the value is inside the propability, meaning it should kill itself
+		'''
 		prosent = MAX_WORM_SEGS/numberOfSegmentsAlive * 100
 		killAnswer = random.randrange(1, 101)
 		
-		print "killanswer and prosent: ", killAnswer, prosent
 		if killAnswer >= prosent:
 			self.killMySelf()
 
 	def shouldIPropagate(self, numberOfSegmentsAlive):
-		#print "number of segs alive and max", numberOfSegmentsAlive, MAX_WORM_SEGS
+		'''
+		Calculates the probability if a segment should propagate, gets a random value from 1 - 100 
+		and checks if the value is inside the propability, meaning it should spread
+		'''
 		prosent = numberOfSegmentsAlive/MIN_WORM_SEGS * 100
 		propagateAnswer = random.randrange(1, 101)
-		
-		print "reproduceanswer and prosent: ", propagateAnswer, prosent
+
 		if propagateAnswer >= prosent:
 			self.propagate()
-
 
 	def listenForIncommingHeartBeats(self):
 		"""
@@ -120,23 +108,22 @@ class WormSegment():
 		thread.start_new_thread(self.udpComm.listen,(10, self.killMySelf, self.updateHeartBeatCount))
 	
 	def get_num_segments(self):
+		'''
+		returns the latest measurement of number of segments alive
+		'''
 		return self.numberOfSegments
 		
 	def killMySelf(self):
 		"""
 		Simply stops all the python threads and quits
 		"""
-		ChangeRunningToFalse()
+		quitPython()
 		
-	def checkIfEmergencyKillSingalIsActive(self):
-		"""
-		Check if the kill file is active
-		"""
-		pass
-
 	def updateHeartBeatCount(self, count):
+		'''
+		Updates heartbeat count
+		'''
 		self.heartbeatreciver += count
-
 
 
 if __name__ == "__main__":
@@ -144,7 +131,6 @@ if __name__ == "__main__":
 	deamonize.daemonize('dev/null', 'output', 'error')
 	os.putenv('DISPLAY', ':0') # Attach to local display
 
-	
 	worm = WormSegment()
 	print "Startign the worm segment"
 	worm.main()
--- cellsgate.py
+++ cellsgate.py
@@ -12,67 +12,81 @@ import pygame
 import random
 
 def deleteAllInFolder(folder):
-	shutil.rmtree(folder)
-	
+    '''
+    Deletes all the files and folders in the given path
+    '''
+    shutil.rmtree(folder)
+        
 class CellGate():
-  def __init__(self):
-    self.numberofwormsstarted = 0
-  
-  def showWormGateWindow(self):
-	pygame.init()
-	x = 150
-	y = 0
-	os.environ['SDL_VIDEO_WINDOW_POS'] = str(x) + ',' + str(y)
-	self.screen = pygame.display.set_mode((150, 150))  
-	SCREEN_COLOR = (random.randrange(0,255), random.randrange(0,255), random.randrange(0,255))
-	color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-	myFont = pygame.font.SysFont("None", 100)
-	while(True):
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.stdout.flush()
-				os._exit(0)
-		pygame.draw.rect(self.screen, (SCREEN_COLOR), (0, 0, self.screen.get_width(), self.screen.get_height()))
-		time_passed = clock.tick(5) # limit to x FPS 
-		self.screen.blit(myFont.render(str(self.numberofwormsstarted), 0, (color)), (10,10))
-		pygame.display.update()
-  
-  def startup(self):
+    '''
+    The worm gate
+    '''
+    def __init__(self):
+    	'''
+    	Constructor
+    	'''
+        self.numberofwormsstarted = 0
     
-    """
-    Start up wormgate
-    """
+    def showWormGateWindow(self):
+    	'''
+    	Displays a pygame window wit random colors displaying the number of segments started
+    	'''
+        pygame.init()
+        x = 150
+        y = 0
+        os.environ['SDL_VIDEO_WINDOW_POS'] = str(x) + ',' + str(y)
+        self.screen = pygame.display.set_mode((150, 150))    
+        SCREEN_COLOR = (random.randrange(0,255), random.randrange(0,255), random.randrange(0,255))
+        color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+        myFont = pygame.font.SysFont("None", 100)
+        while(True):
+                for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                                sys.stdout.flush()
+                                os._exit(0)
+                pygame.draw.rect(self.screen, (SCREEN_COLOR), (0, 0, self.screen.get_width(), self.screen.get_height()))
+                time_passed = clock.tick(5) # limit to x FPS 
+                self.screen.blit(myFont.render(str(self.numberofwormsstarted), 0, (color)), (10,10))
+                pygame.display.update()
+    
+    def startup(self):  
+        """
+        Start up wormgate
+        """
 
-    if os.path.exists(TMP_FOLDER):
-    	deleteAllInFolder(TMP_FOLDER);
-	
-	os.makedirs(TMP_FOLDER)
-    
-   	deamonize.daemonize('dev/null', TMP_FOLDER + 'output', TMP_FOLDER + 'error')
-    thread.start_new_thread(self.die,())
-    
-    thread.start_new_thread(self.showWormGateWindow, ())
-    
-    self.fileserver = communication.FileServer(LISTEN_PORT, WORM_GATE_PORT)
-    self.fileserver.main(self.setNumberOfStartedSegmets)
-    
-  def setNumberOfStartedSegmets(self, number):
-	self.numberofwormsstarted = number
-	  
-  def die(self):
-    """
-    Stop worm gate in case of big emergencies
-    """
-    while(True):
-		rows = getoutput("pgrep -f cells").split("\n")
-		if len(rows) > (MAX_WORM_SEGS * 2):
-			getoutput("killall python")
-		# TODO kill wormgate
-		time.sleep(0.5)
-	
+        if os.path.exists(TMP_FOLDER):
+                deleteAllInFolder(TMP_FOLDER);
+        
+        os.makedirs(TMP_FOLDER)
+        
+        deamonize.daemonize('dev/null', TMP_FOLDER + 'output', TMP_FOLDER + 'error')
+        thread.start_new_thread(self.die,())
+        
+        thread.start_new_thread(self.showWormGateWindow, ())
+        
+        self.fileserver = communication.FileServer(LISTEN_PORT, WORM_GATE_PORT)
+        self.fileserver.main(self.setNumberOfStartedSegmets)
+        
+    def setNumberOfStartedSegmets(self, number):
+    	'''
+    	Sets the total number of startet segments
+    	'''
+        self.numberofwormsstarted = number
+            
+    def die(self):
+        """
+        Stop worm gate in case of big emergencies
+        """
+        while(True):
+                rows = getoutput("pgrep -f cells").split("\n")
+                if len(rows) > (MAX_WORM_SEGS * 2):
+                        getoutput("killall python")
+                # TODO kill wormgate
+                time.sleep(0.5)
+        
 
 
 if __name__ == "__main__":
-	cellgate = CellGate()
-	cellgate.startup()
-	
+        cellgate = CellGate()
+        cellgate.startup()
+        
