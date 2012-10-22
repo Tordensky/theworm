7a59140982e1957509664f5978de64815c144135
Alexander Svendsen <asv009@ifilab122.stud.cs.uit.no>
2012-10-22 15:57:14 +0200

    added the mutex class

---
+++ mutex.py
@@ -0,0 +1,12 @@
+
+
+#The mutex only supports one critical section, for simplicity.The cricial section should always be the same place, may be extended in the future
+class Mutex():
+	def __init__(self):
+		pass
+	
+	def lock(self):
+		pass
+	
+	def unlock(self):
+		pass
